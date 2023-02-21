1a6b7e3dc5eea28441ed56efe3e7477aee653469
ysknk <31568245+ysknk@users.noreply.github.com>
2023-02-21 15:17:59 +0900

    Create .env.default.ts

---
+++ .env.default.ts
@@ -0,0 +1,15 @@
+export default {
+  app: {
+    baseURL: '/',
+
+    head: {
+      charset: 'utf-8',
+      title: 'example title',
+      viewport: 'width=device-width, initial-scale=1',
+      meta: [
+        { name: 'format-detection', content: 'telephone=no' },
+        { name: 'description', content: 'example description' }
+      ],
+    }
+  },
+}
