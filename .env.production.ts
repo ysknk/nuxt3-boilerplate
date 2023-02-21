63e4034ea90948f4382ffc9bda9d6dd1df7188ea
ysknk <31568245+ysknk@users.noreply.github.com>
2023-02-21 15:18:54 +0900

    Create .env.production.ts

---
+++ .env.production.ts
@@ -0,0 +1,11 @@
+export default {
+  app: {
+    // baseURL: '/test/'
+  },
+  runtimeConfig: {
+    public: {
+      appEnv: 'production',
+      apiPath: 'https://example.com/api/',
+    },
+  }
+}
