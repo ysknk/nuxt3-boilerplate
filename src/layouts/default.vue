478a8631e9e596b7ad60323d8e7d9ccacc1e456f
ysknk <31568245+ysknk@users.noreply.github.com>
2023-03-03 12:10:52 +0900

    Update default.vue

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -11,4 +11,12 @@
   display block
   relative top 0 left 0
   background $test
+
+.page-enter-active,
+.page-leave-active
+  transition all 0.4s
+.page-enter-from,
+.page-leave-to
+  opacity 0
+  filter blur(1rem)
 </style>
