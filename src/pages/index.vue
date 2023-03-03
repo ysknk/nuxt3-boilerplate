3a971bf28a47f71f8bda5a57dfb8953a1568f0ac
ysknk <31568245+ysknk@users.noreply.github.com>
2023-03-03 12:08:57 +0900

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -19,18 +19,19 @@ useSEO({
 </script>
 
 <template lang="pug">
-p env: {{ $config.public.appEnv }}
-p width: {{ $window.width }}
-p height: {{ $window.height }}
-p scrolltop: {{ $window.scrollTop }}
+div(ref="root")
+  p env: {{ $config.public.appEnv }}
+  p width: {{ $window.width }}
+  p height: {{ $window.height }}
+  p scrolltop: {{ $window.scrollTop }}
 
-div(v-if="result")
-  p data: {{ result }}
-div(v-else)
-  p data: loading
-p: NuxtLink(to="/simple/") simple
-//- p: NuxtLink(to="/test/") test
-//- NuxtWelcome
+  div(v-if="result")
+    p data: {{ result }}
+  div(v-else)
+    p data: loading
+  p: NuxtLink(to="/simple/") simple
+  //- p: NuxtLink(to="/test/") test
+  //- NuxtWelcome
 </template>
 
 <style lang="stylus" scoped>
